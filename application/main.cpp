9b9d30f782aa68fba41a81a4aab89c7934cf02d7
Ashton Sickler <ashton.sickler@icloud.com>
2024-11-07 14:45:40 -0600

    Added more Flags; TODO: Better parsing of Flags

--- application/main.cpp
+++ application/main.cpp
@@ -27,18 +27,10 @@ void usage(char **argv) {
     std::cout << "Usage: " << argv[0] << " <flags> <path_to_file>" << std::endl;
     std::cout << "Flags:" << std::endl;
     for (int i = 0; i < flags.size(); i++) {
+        std::cout << "    ";
         flags[i].print();
     }
     std::cout << std::endl;
-}
-
-void print_tokens(vector<Token> tokens) {
-    int total = 0;
-    for (int i = 0; i < tokens.size(); ++i) {
-        tokens[i].print();
-        total++;
-    }
-    std::cout << "Total tokens: " << total << std::endl;
 }
 
 void debug_tokens(std::vector<Token> tokens) {
@@ -49,6 +41,12 @@ void debug_tokens(std::vector<Token> tokens) {
         total++;
     }
     std::cout << "Total tokens: " << total << std::endl;
+}
+
+void debug_flags() {
+    for (int i = 0; i < flags.size(); i++) {
+        flags[i].print();
+    }
 }
 
 string get_file_contents(string file_path) {
@@ -65,9 +63,9 @@ int main(int argc, char **argv) {
 
     string path;
 
-    Flag debug_flag = Flag({"-d", "-debug"}, "DEBUG", false);
-
-    flags.push_back(Flag({"-d", "-debug"}, "DEBUG", false));
+    flags.push_back(Flag({"-c", "-compile", "-build"}, "Compiles the project | (Not impliemented yet)", (void*)false));
+    flags.push_back(Flag({"-d", "-debug"}, "DEBUG", (void*)false));
+    flags.push_back(Flag({"-s", "-silent"}, "Silences the compiler | (Not implemented yet)", (void*)false));
 
     if (argc < 2) {
         usage(argv);
@@ -79,12 +77,12 @@ int main(int argc, char **argv) {
     for (int arg_idx = 0; arg_idx < argc; arg_idx++) {
         for (int flag_idx = 0; flag_idx < flags.size(); flag_idx++) {
             if (flags[flag_idx].has(argv[arg_idx])) {
-                std::cout << "Found: " << argv[arg_idx] << std::endl;
-                flags[flag_idx].update();
+                // std::cout << "Found: " << argv[arg_idx] << std::endl;
+                flags[flag_idx].update((void*)1);
             }
         }
     }
 
-    vector<Token> words = tokenize_with_positions(path);
+    // vector<Token> words = tokenize_with_positions(path);
     // debug_tokens(words);
 }
