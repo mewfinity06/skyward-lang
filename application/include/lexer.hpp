1f877d7b06b8e81b01cbec5a0e35c90538c30b61
Mute <rsteinke1111@gmail.com>
2024-11-07 04:16:27 +0100

    Cleaned up string token to enum translation in Token::check_type()

--- application/include/lexer.hpp
+++ application/include/lexer.hpp
@@ -1,10 +1,11 @@
 #pragma once
+#include "common.hpp"
+#include "error.hpp"
 #include <cctype>
 #include <unordered_set>
 #include <sstream>
 #include <fstream>
-#include "common.hpp"
-#include "error.hpp"
+#include <variant>
 
 // Forward declarations
 class Token;
@@ -32,7 +33,7 @@ const std::unordered_set<std::string> keywords = {
     "signed", "unsigned", "use", "as"
 };
 
-enum class TokenKind {
+enum TokenKind {
     // Multichar tokens
     TOKEN_IDENT,
     TOKEN_INT,
@@ -100,6 +101,8 @@ enum class TokenKind {
     TOKEN_UNKNOWN,
 };
 
+
+
 // Checkers
 bool can_convert_to_int(const string& str);
 
@@ -121,6 +124,54 @@ string token_kind_to_string(TokenKind kind);
 vector<Token> tokenize_with_positions(const std::string file_path);
 
 class Token {
+private:
+    const static inline std::unordered_map<string, TokenKind> string_token_to_enum_dictionary = {
+        {"*", TOKEN_STAR},
+        {"/", TOKEN_DIVIDE},
+        {"+", TOKEN_PLUS},
+        {"-", TOKEN_MINUS},
+        {"%", TOKEN_MOD},
+        {"=", TOKEN_ASSIGNMENT},
+        {":=", TOKEN_IMPLICIT_ASSIGNMENT},
+        {"(", TOKEN_OPEN_PAREN},
+        {")", TOKEN_CLOSE_PAREN},
+        {"[", TOKEN_OPEN_BRACKET},
+        {"]", TOKEN_CLOSE_BRACKET},
+        {"{", TOKEN_OPEN_CURLY},
+        {"}", TOKEN_CLOSE_CURLY},
+        {"!", TOKEN_BANG},
+        {"@", TOKEN_ABROSE},
+        {"#", TOKEN_POUND},
+        {"^", TOKEN_CARROT},
+        {"&", TOKEN_AMPERSAND},
+        {"|", TOKEN_PIPE},
+        {"<", TOKEN_LEFT_ARROW},
+        {">", TOKEN_RIGHT_ARROW},
+        {":", TOKEN_COLON},
+        {";", TOKEN_SEMI_COLON},
+        {",", TOKEN_COMMA},
+        {".", TOKEN_FULL_STOP},
+        {"?", TOKEN_QUESTION},
+        {"`", TOKEN_BACK_TICK},
+        {"~", TOKEN_TILDA},
+        {"\\", TOKEN_BACK_SLASH},
+        {"==", TOKEN_EQUALS},
+        {"!=", TOKEN_NOT_EQUALS},
+        {">=", TOKEN_GREATER_EQUALS},
+        {"<=", TOKEN_LESSER_EQUALS},
+        {"=>", TOKEN_FAT_ARROW},
+        {"->", TOKEN_THIN_ARROW},
+        {"|>", TOKEN_PIPE_LINE},
+        {"++", TOKEN_PLUS_PLUS},
+        {"--", TOKEN_MINUS_MINUS},
+        {"**", TOKEN_STAR_STAR},
+        {"&&", TOKEN_AND},
+        {"||", TOKEN_OR},
+        {"END OF FILE", TOKEN_EOF}
+    };
+
+protected:
+   TokenKind translate_token_kind_from_string(string token);
 public:
     TokenKind kind;
     string word;
