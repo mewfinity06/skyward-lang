1ef853872accfb850a44ce792d345ff3bd6c0275
Ashton Sickler <ashton.sickler@icloud.com>
2024-11-08 10:23:58 -0600

    Added new tokens; Added TODOs

--- application/include/lexer.hpp
+++ application/include/lexer.hpp
@@ -10,28 +10,34 @@
 // Forward declarations
 class Token;
 
-const std::string single_char_delims = "!@#$%^&*()_+-={}[]|\\:;\"\'<>,.?/`~";
+const std::string single_char_delims = "!@#$%^&*()+-={}[]|\\:;\"\'<>,.?/`~";
 
 const std::string comment_start = "//";
 
+// TODO: 
+//     | Impliment non_word_tokens instead of multi_char_tokens
+//     | Better name?
 const std::unordered_set<std::string> non_word_tokens = {
     // Assignment
     ":", "=", ":=", "+=", "-=", "*=", "/=", "%=", "|=", "~=",
-    "&=", "&~=", "<<=", ">==", "&&=", "||=",
+    "&=", "&~=", "<<=", ">>=", "&&=", "||=",
     // Seperators
     "{", "}", "(", ")", "[", "]", ";", ",", 
     // Iterators
     "..<", "..=", 
     // Comparison
-    "==", "!=", "<=", ">=",
+    "==", "!=", "<=", ">=", "<", ">", 
     // Logical operators
     "&&", "||", "!", "|", "~", "&", "&~", "<<", ">>", 
     // Arithmetic
     "+", "-", "*", "%", "++", "--", "**",
+    // Arrows
+    "=>", "->", "|>",
     // Misc
-    "<", ">", "@", "#", "$", "^"
+    "@", "#", "$", "^", ".", "`", "\\", "?"
 };
 
+// TODO: Depricate this, impliment non_word_tokens
 const std::unordered_set<std::string> multi_char_tokens = {
     "<=", ">=", "==", "!=", "&&", "||", "++", "--", "**",
     "->", "=>", "||", "&&", "|>", ":=",
@@ -55,6 +61,7 @@ enum TokenKind {
     // Multichar tokens
     TOKEN_IDENT,
     TOKEN_INT,
+    TOKEN_FLOAT, // TODO: Add support for floats
 
     TOKEN_KEYWORD,
     TOKEN_TYPE,
@@ -62,15 +69,23 @@ enum TokenKind {
     TOKEN_STRING,
     TOKEN_CHAR,
 
-    // Binary Tokens
-    TOKEN_PLUS,
-    TOKEN_MINUS,
-    TOKEN_STAR,
-    TOKEN_DIVIDE,
-    TOKEN_MOD,
-
-    TOKEN_ASSIGNMENT,          // =
-    TOKEN_IMPLICIT_ASSIGNMENT, // :=
+    // Assignment tokens
+    TOKEN_ASSIGNMENT,                     // =
+    TOKEN_COLON,                          // :  | Used for typing 
+    TOKEN_ASSIGNMENT_IMPLICIT,            // :=
+    TOKEN_ASSIGNMENT_PLUS,                // +=
+    TOKEN_ASSIGNMENT_MINUS,               // -=
+    TOKEN_ASSIGNMENT_STAR,                // *=
+    TOKEN_ASSIGNMENT_DIVIDE,              // /=
+    TOKEN_ASSIGNMENT_MOD,                 // %=
+    TOKEN_ASSIGNMENT_BITWISE_OR,          // |=
+    TOKEN_ASSIGNMENT_BITWISE_AND,         // &=
+    TOKEN_ASSIGNMENT_BITWISE_XOR,         // ~=
+    TOKEN_ASSIGNMENT_BITWISE_AND_NOT,     // &~=
+    TOKEN_ASSIGNMENT_BITWISE_SHIFT_LEFT,  // <<=
+    TOKEN_ASSIGNMENT_BITWISE_SHIFT_RIGHT, // >>=
+    TOKEN_ASSIGNMENT_CONDITIONAL_AND,     // &&= | a &&= b -> a = a && b
+    TOKEN_ASSIGNMENT_CONDITIONAL_OR,      // ||= | a ||= b -> a = a || b 
 
     // Seperator tokens
     TOKEN_OPEN_PAREN,
@@ -79,41 +94,56 @@ enum TokenKind {
     TOKEN_CLOSE_BRACKET,
     TOKEN_OPEN_CURLY,
     TOKEN_CLOSE_CURLY,
+    TOKEN_SEMI_COLON,
+    TOKEN_COMMA,
 
-    // Single char tokens
-    TOKEN_BANG,        // !
-    TOKEN_ABROSE,      // @
-    TOKEN_POUND,       // #
-    TOKEN_CARROT,      // ^
-    TOKEN_AMPERSAND,   // &
-    TOKEN_PIPE,        // |
-    TOKEN_LEFT_ARROW,  // <
-    TOKEN_RIGHT_ARROW, // >
-    TOKEN_COLON,       // :
-    TOKEN_SEMI_COLON,  // ;
-    TOKEN_COMMA,       // ,
-    TOKEN_FULL_STOP,   // .
-    TOKEN_QUESTION,    // ?
-    TOKEN_BACK_TICK,   // `
-    TOKEN_TILDA,       // ~
-    TOKEN_BACK_SLASH,
+    // Iterators
+    TOKEN_TO_LESS,  // ..<
+    TOKEN_TO_EQUAL, // ..=
 
-    // Multichar non alphanumeric tokens
+    // Comparison
     TOKEN_EQUALS,         // ==
     TOKEN_NOT_EQUALS,     // !=
     TOKEN_GREATER_EQUALS, // >=
     TOKEN_LESSER_EQUALS,  // <=
+    TOKEN_LEFT_ARROW,     // <
+    TOKEN_RIGHT_ARROW,    // >
 
+    // Logical operators
+    TOKEN_AND,         // &&
+    TOKEN_OR,          // ||
+    TOKEN_BANG,        // !
+    TOKEN_PIPE,        // |
+    TOKEN_TILDA,       // ~
+    TOKEN_AMPERSAND,   // &
+    TOKEN_AND_NOT,     // &~
+    TOKEN_SHIFT_LEFT,  // <<
+    TOKEN_SHIFT_RIGHT, // >>
+
+    // Binary Tokens
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+    TOKEN_STAR,
+    TOKEN_DIVIDE,
+    TOKEN_MOD,
+    TOKEN_PLUS_PLUS,
+    TOKEN_MINUS_MINUS,
+    TOKEN_STAR_STAR,
+
+    // Arrows
     TOKEN_FAT_ARROW,  // =>
     TOKEN_THIN_ARROW, // ->
     TOKEN_PIPE_LINE,  // |> (Thanks Gleam!)
 
-    TOKEN_PLUS_PLUS,   // ++
-    TOKEN_MINUS_MINUS, // --
-    TOKEN_STAR_STAR,   // **
-
-    TOKEN_AND, // &&
-    TOKEN_OR,  // ||
+    // Single char tokens
+    TOKEN_ABROSE,      // @
+    TOKEN_POUND,       // #
+    TOKEN_DOLLAR,      // $
+    TOKEN_CARROT,      // ^
+    TOKEN_FULL_STOP,   // .
+    TOKEN_QUESTION,    // ?
+    TOKEN_BACK_TICK,   // `
+    TOKEN_BACK_SLASH,
 
     TOKEN_EOF,
     TOKEN_UNKNOWN,
@@ -144,47 +174,74 @@ vector<Token> tokenize_with_positions(const std::string file_path);
 class Token {
 private:
     const static inline std::unordered_map<string, TokenKind> string_token_to_enum_dictionary = {
-        {"*", TOKEN_STAR},
-        {"/", TOKEN_DIVIDE},
-        {"+", TOKEN_PLUS},
-        {"-", TOKEN_MINUS},
-        {"%", TOKEN_MOD},
         {"=", TOKEN_ASSIGNMENT},
-        {":=", TOKEN_IMPLICIT_ASSIGNMENT},
+        {":", TOKEN_COLON},
+        {":=", TOKEN_ASSIGNMENT_IMPLICIT},
+        {"+=", TOKEN_ASSIGNMENT_PLUS},
+        {"-=", TOKEN_ASSIGNMENT_MINUS},
+        {"*=", TOKEN_ASSIGNMENT_STAR},
+        {"/=", TOKEN_ASSIGNMENT_DIVIDE},
+        {"%=", TOKEN_ASSIGNMENT_MOD},
+        {"|=", TOKEN_ASSIGNMENT_BITWISE_OR},
+        {"&=", TOKEN_ASSIGNMENT_BITWISE_AND},
+        {"~=", TOKEN_ASSIGNMENT_BITWISE_XOR},
+        {"&~=", TOKEN_ASSIGNMENT_BITWISE_AND_NOT},
+        {"<<=", TOKEN_ASSIGNMENT_BITWISE_SHIFT_LEFT},
+        {">>=", TOKEN_ASSIGNMENT_BITWISE_SHIFT_RIGHT},
+        {"&&=", TOKEN_ASSIGNMENT_CONDITIONAL_AND},
+        {"||=", TOKEN_ASSIGNMENT_CONDITIONAL_OR},
+
         {"(", TOKEN_OPEN_PAREN},
         {")", TOKEN_CLOSE_PAREN},
         {"[", TOKEN_OPEN_BRACKET},
         {"]", TOKEN_CLOSE_BRACKET},
         {"{", TOKEN_OPEN_CURLY},
         {"}", TOKEN_CLOSE_CURLY},
-        {"!", TOKEN_BANG},
-        {"@", TOKEN_ABROSE},
-        {"#", TOKEN_POUND},
-        {"^", TOKEN_CARROT},
-        {"&", TOKEN_AMPERSAND},
-        {"|", TOKEN_PIPE},
-        {"<", TOKEN_LEFT_ARROW},
-        {">", TOKEN_RIGHT_ARROW},
-        {":", TOKEN_COLON},
         {";", TOKEN_SEMI_COLON},
         {",", TOKEN_COMMA},
-        {".", TOKEN_FULL_STOP},
-        {"?", TOKEN_QUESTION},
-        {"`", TOKEN_BACK_TICK},
-        {"~", TOKEN_TILDA},
-        {"\\", TOKEN_BACK_SLASH},
+
+        {"..<", TOKEN_TO_LESS},
+        {"..=", TOKEN_TO_EQUAL},
+
         {"==", TOKEN_EQUALS},
         {"!=", TOKEN_NOT_EQUALS},
         {">=", TOKEN_GREATER_EQUALS},
         {"<=", TOKEN_LESSER_EQUALS},
-        {"=>", TOKEN_FAT_ARROW},
-        {"->", TOKEN_THIN_ARROW},
-        {"|>", TOKEN_PIPE_LINE},
+        {"<", TOKEN_LEFT_ARROW},
+        {">", TOKEN_RIGHT_ARROW},
+
+        {"&&", TOKEN_AND},
+        {"||", TOKEN_OR},
+        {"!", TOKEN_BANG},
+        {"|", TOKEN_PIPE},
+        {"~", TOKEN_TILDA},
+        {"&", TOKEN_AMPERSAND},
+        {"&~", TOKEN_AND_NOT},
+        {"<<", TOKEN_SHIFT_LEFT},
+        {">>", TOKEN_SHIFT_RIGHT},
+
+        {"+", TOKEN_PLUS},
+        {"-", TOKEN_MINUS},
+        {"*", TOKEN_STAR},
+        {"/", TOKEN_DIVIDE},
+        {"%", TOKEN_MOD},
         {"++", TOKEN_PLUS_PLUS},
         {"--", TOKEN_MINUS_MINUS},
         {"**", TOKEN_STAR_STAR},
-        {"&&", TOKEN_AND},
-        {"||", TOKEN_OR},
+
+        {"=>", TOKEN_FAT_ARROW},
+        {"->", TOKEN_THIN_ARROW},
+        {"|>", TOKEN_PIPE_LINE},
+        
+        {"@", TOKEN_ABROSE},
+        {"#", TOKEN_POUND},
+        {"$", TOKEN_DOLLAR},
+        {"^", TOKEN_CARROT},
+        {".", TOKEN_FULL_STOP},
+        {"`", TOKEN_BACK_TICK},
+        {"\\", TOKEN_BACK_SLASH},
+        {"?", TOKEN_QUESTION},
+        
         {"END OF FILE", TOKEN_EOF}
     };
 
